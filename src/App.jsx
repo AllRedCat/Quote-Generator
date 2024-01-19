0c0bd8ec6e48f9cfc9941a370be1c2ac61269290
AllRedCat <leonismogabriel@gmail.com>
2024-01-19 00:58:13 -0300

    Add the system

---
+++ src/App.jsx
@@ -0,0 +1,39 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
+const App = () => {
+  const [quote, setQuote] = useState('');
+  const [author, setAuthor] = useState('');
+
+  useEffect(() => {
+    fetchQuote();
+  }, []);
+
+  const fetchQuote = async () => {
+    try {
+      const response = await axios.get('https://api.quotable.io/random');
+      const { content, author } = response.data;
+      setQuote(content);
+      setAuthor(author);
+    } catch (error) {
+      console.error('Erro ao obter citação:', error.message);
+    }
+  };
+
+  const handleNewQuote = () => {
+    fetchQuote();
+  };
+
+  return (
+    <div className="App">
+      <div>
+        <h2>Citação:</h2>
+        <p>{quote}</p>
+        <p>- {author}</p>
+      </div>
+      <button onClick={handleNewQuote}>Nova Citação</button>
+    </div>
+  );
+};
+
+export default App;
